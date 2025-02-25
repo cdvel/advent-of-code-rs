044ebd32f46c672a2e0e9f8877a25e7f8a6e9a1d
Cesar D. Velandia <cesar@velandia.co>
2025-02-25 11:58:31 +0800

    Add day7 pt2

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,10 +1,10 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 #[derive(Debug)]
 struct BagRule {
-    // quantity: u32,
+    quantity: u32,
     color: String,
 }
 
@@ -20,34 +20,28 @@ fn read_entries(line: &str) -> (String, Vec<BagRule>) {
         .split(", ")
         .map(|rule| {
             let words: Vec<&str> = rule.split_whitespace().collect();
-            // let quantity = words[0].parse().unwrap();
+            let quantity = words[0].parse().unwrap();
             let color = format!("{} {}", words[1], words[2]);
-            BagRule {  color }
+            BagRule { quantity, color }
         })
         .collect();
 
     (container, contents)
 }
 
-fn can_contain_shiny_gold(
-    color: &str,
-    rules: &HashMap<String, Vec<BagRule>>,
-    memo: &mut HashSet<String>
-) -> bool {
-    if memo.contains(color) {
-        return false;
-    }
+fn count_bags_inside(color: &str, rules: &HashMap<String, Vec<BagRule>>) -> u32 {
+    let mut total = 0;
     
     if let Some(contents) = rules.get(color) {
         for bag in contents {
-            if bag.color == "shiny gold" || can_contain_shiny_gold(&bag.color, rules, memo) {
-                return true;
-            }
+            // Add the bags directly inside
+            total += bag.quantity;
+            // Add the bags inside each of those bags
+            total += bag.quantity * count_bags_inside(&bag.color, rules);
         }
     }
     
-    memo.insert(color.to_string());
-    false
+    total
 }
 
 fn main() {
@@ -56,22 +50,15 @@ fn main() {
     
     let mut rules: HashMap<String, Vec<BagRule>> = HashMap::new();
     
+    // Parse all rules
     for line in reader.lines() {
         let line = line.unwrap();
         let (container, contents) = read_entries(&line);
         rules.insert(container, contents);
     }
     
-    let mut count = 0;
-    let mut memo = HashSet::new();
-    
-    for color in rules.keys() {
-        if can_contain_shiny_gold(color, &rules, &mut memo) {
-            count += 1;
-        }
-    }
-    
-    println!("Number of bag colors that can contain a shiny gold bag: {}", count);
+    let total_bags = count_bags_inside("shiny gold", &rules);
+    println!("Number of bags required inside a single shiny gold bag: {}", total_bags);
 }
 
 
